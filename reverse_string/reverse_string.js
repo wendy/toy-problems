396b0445cf5814656b139919d0af79e76a7c33a2
Wendy Leung <wendysarahleung@gmail.com>
2015-06-15 13:49:20 -0700

    (feat) Add & Complete reverse_string

---
+++ reverse_string/reverse_string.js
@@ -0,0 +1,13 @@
+//Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+
+var reverse = function(string) {
+  var result = '';
+
+  for( var i = string.length - 1; i >= 0; i-- ){
+    result += string[i];
+  }
+
+  return result;
+};
+
+reverse('test');
